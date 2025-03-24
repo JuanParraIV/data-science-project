f5bda9951b80bbf67c238386f18b484ae0660e3f
JuanParraIV <jmparra1993@gmail.com>
2025-03-24 15:42:36 -0500

    Implement data ingestion pipeline with configuration management and logging

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from src.datascienceproject.logging import logger
+from src.datascienceproject.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 
-logger.info("Welcome to our custom logging module!")
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.initiate_data_ingestion()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
